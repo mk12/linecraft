a51b84444aed514728147c4bddfecfd5e547b6dd
Mitchell Kember <mk12360@gmail.com>
2015-08-28 00:16:12 -0400

    Can't remember what I was doing here

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ var stage;
 var renderer;
 var graphics;
 
+var control = {left: false, right: false, down: false, up: false};
+
 (function () {
 	setupPixi();
+	addRendererView();
 	createSquare();
-	beginAnimation();
+	registerEvents();
+	requestAnimFrame(animate);
 })();
 
 // Initializes the stage, renderer, and the graphics objects.
@@ -16,10 +20,9 @@ function setupPixi() {
 	graphics = new PIXI.Graphics();
 }
 
-// Adds the renderer view to the DOM and begins rendering.
-function beginAnimation() {
+// Adds the renderer view to the DOM.
+function addRendererView() {
 	document.getElementById('main').appendChild(renderer.view);
-	requestAnimFrame(animate);
 }
 
 // Returns the pixel ratio (for Retina devices, this is 2).
@@ -39,11 +42,35 @@ function createSquare() {
 	stage.addChild(graphics);
 }
 
+// Registers events for handling keyboard input.
+function registerEvents() {
+	var keymap = {};
+	keymap[65] = 'left';
+	keymap[68] = 'right';
+	keymap[83] = 'down';
+	keymap[87] = 'up';
+	document.addEventListener('keydown', function(e) {
+		if (e.keyCode in keymap) {
+			control[keymap[e.keyCode]] = true;
+		}
+	});
+	document.addEventListener('keyup', function(e) {
+		if (e.keyCode in keymap) {
+			control[keymap[e.keyCode]] = false;
+		}
+	});
+}
+
 // Updates the simulation and renders one frame.
 function animate() {
 	requestAnimFrame(animate);
-	graphics.rotation += 0.005;
-	var s = 3*Math.sin(graphics.rotation);
+	if (control.up) {
+		console.log("up");
+		graphics.rotation += 0.05;
+	} else if (control.down) {
+		graphics.rotation -= 0.05;
+	}
+	var s = 3;//3*Math.sin(graphics.rotation);
 	graphics.scale = new PIXI.Point(s, s);
 	renderer.render(stage);
 }
